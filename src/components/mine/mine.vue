46525355559d1984a41c4b57f4f96d7640c056c0
JaneChelle <2962784374@qq.com>
2021-04-15 11:07:26 +0800

    style：添加空图片

--- src/components/mine/mine.vue
+++ src/components/mine/mine.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: JaneChelle
  * @Date: 2019-10-24 15:55:29
- * @LastEditTime: 2021-04-15 10:16:46
+ * @LastEditTime: 2021-04-15 11:05:47
  * @Description:
 -->
 <template>
@@ -13,30 +13,66 @@
       </div>
       <h1 class="heading">收藏</h1>
     </div>
-    <div class="mine-result">
-      <div>内容</div>
-    </div>
+    <scroll :data="songs" @scroll="scroll" ref="list" class="list"
+              :listen-scroll="listenScroll"
+              :probe-type="probeType">
+        <div class="song-list-wrapper">
+          <song-list @select="selectItem" :songs="songs"></song-list>
+        </div>
+        <div v-show="!songs.length" class="loading-container">
+          <!-- <loading></loading> -->
+          <img src="../../assets/null.png" alt="" class="img-null">
+        </div>
+    </scroll>
     <router-view></router-view>
   </div>
 </transition>
 </template>
 
 <script>
+  import Scroll from 'base/scroll/scroll'
+  import SongList from 'base/song-list/song-list'
+  import Loading from 'base/loading/loading'
+  import {mapActions} from 'vuex'
   export default {
-    name: 'search',
+    name: 'mine',
     created () {
+      this.probeType = 3
+      this.listenScroll = true
     },
     data () {
       return {
-        query: ''
+        songs: [],
+        scrollY: 0
       }
     },
     methods: {
+      scroll(pos) {
+        this.scrollY = pos.y
+      },
       back() {
         this.$router.replace('/recommend')
-      }
+      },
+      selectItem (item, index) {
+        this.selectPlay({
+          list: this.songs,
+          index
+        })
+      },
+      random () {
+        this.randomPlay({
+          list: this.songs
+        })
+      },
+      ...mapActions([
+        'selectPlay',
+        'randomPlay'
+      ])
     },
     components: {
+      Scroll,
+      SongList,
+      Loading
     }
   }
 </script>
@@ -76,13 +112,28 @@
         top 0
         left 0
         right 0
+        z-index 40
         text-align center
         white-space nowrap
         line-height 40px
         font-size 18px
-    .mine-result
-      position: fixed
-      width: 100%
-      top: 80px
-      bottom: 0
+        color: #000
+    .list
+      position fixed
+      top 0
+      bottom 0
+      width 100%
+      background $color-background
+      /*overflow hidden*/
+      .song-list-wrapper
+        padding 20px 30px
+      .loading-container
+        position absolute
+        width 100%
+        top 50%
+        transform translateY(-50%)
+        .img-null
+          display inline-block
+          width: 100%
+          transform scale(0.5)
 </style>
